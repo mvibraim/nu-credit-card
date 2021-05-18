c60eab0295419b45a9fae6520880955704a28385
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2021-05-18 03:02:27 -0300

    Create a database connection

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.datomic/datomic-pro "1.0.6269"]]
   :main ^:skip-aot nu-credit-card.core
   :target-path "target/%s"
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
-                                   :creds :gpg}}
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
